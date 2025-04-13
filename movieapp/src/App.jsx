8250c184132224ae1b6013ad8893dfa727c3c908
Phoenix Li <xintong.li@mail.mcgill.ca>
2025-04-13 18:40:33 -0400

    initial commit

---
+++ movieapp/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() { // this is a component
+  return (
+    <>
+    <Text />
+    <Text />
+    </>
+  )
+}
+
+function Text() {
+  return <div>
+    <p>hello world</p>
+  </div>
+}
+
+
+export default App
