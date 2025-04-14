94efb4ddc1e0df35da3b882eb026e5b486c60683
Phoenix Li <xintong.li@mail.mcgill.ca>
2025-04-13 22:49:19 -0400

    finished all components and pages

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,14 +1,27 @@
-import './App.css'
+import './css/App.css'
 import MovieCard from './components/MovieCard'
+import Favorites from './pages/Favorites'
 import Home from './pages/Home'
+import {Routes, Route} from "react-router-dom" // you need this to use router
+import NavBar from './components/NavBar'
 
 function App() { // this is a component
   return (
-    <>
-    <Home></Home>
-    </>
-  )
+    <div>
+      <NavBar></NavBar>
+      <main className="main-content">
+        <Routes>
+          <Route path = "/" element = {<Home />}></Route>
+          <Route path = "/favorites" element = {<Favorites />}></Route>
+        </Routes>
+      </main>
+    </div>
+  );
 }
+
+
+export default App;
+
 
 //things below can be put into the function app body, just for study purpose
 //<MovieCard movie = {{title:"Lily", release_date:"2024"}} />
@@ -18,6 +31,3 @@ function App() { // this is a component
     //<p>{display}</p>
   //</div>
 //}
-
-
-export default App
